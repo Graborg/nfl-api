0c6bcd17a28aedfcb7f30fbd1df32ca459866831
Mikael Gråborg <mikael.graborg@iteam.se>
2018-10-07 13:46:46 +0200

    fix: sort out cors settings

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const { registerRoutes } = require('./routes')
 const port  = require('./config').get('port')
 
 const app = express()
-const cors = require('cors')
-app.use(cors())
 
 app.use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Origin", "http://localhost:3333")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, Set-Cookie")
+    res.header("Access-Control-Allow-Credentials", "true")
     next()
 })
 
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 registerRoutes(app)
 app.listen(port, () => {
