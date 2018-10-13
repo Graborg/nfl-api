e62f8af6a7147b8dd4389ed107469b6e0a76bb85
Mikael Gråborg <mikael.graborg@iteam.se>
2018-10-13 21:45:36 +0200

    refactor: eslint everything!

--- config.js
+++ config.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const nconf = require('nconf')
 
 nconf.file({
-    file: 'config.json',
-    dir: '../',
-    search: true
+  file: 'config.json',
+  dir: '../',
+  search: true
 })
 nconf.argv()
-    .env()
+  .env()
 
 nconf.defaults({
-    rethinkdb: {
-        host: "localhost"
-    },
-    sportRadar: {
-        api_key: ''
-    },
-    env: 'DEVELOP',
-    'port': 7331,
-    frontendDomain: 'http://localhost:3333'
+  rethinkdb: {
+    host: 'localhost'
+  },
+  sportRadar: {
+    api_key: ''
+  },
+  env: 'DEVELOP',
+  'port': 7331,
+  frontendDomain: 'http://localhost:3333'
 })
 
 if (fs.existsSync('/run/secrets/sportRadarApiKey')) {
-    nconf.set('sportRadar:api_key', fs.readFileSync('/run/secrets/sportRadarApiKey', 'utf8'))
+  nconf.set('sportRadar:api_key', fs.readFileSync('/run/secrets/sportRadarApiKey', 'utf8'))
 }
 module.exports = nconf
--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const frontendDomain = config.get('frontendDomain')
 const app = express()
 
 app.use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", frontendDomain)
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
-    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, Set-Cookie")
-    res.header("Access-Control-Allow-Credentials", "true")
-    next()
+  res.header('Access-Control-Allow-Origin', frontendDomain)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, Set-Cookie')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
 })
 
 app.use(bodyParser.json())
@@ -20,5 +20,5 @@ app.use(cookieParser())
 
 registerRoutes(app)
 app.listen(port, () => {
-    console.log('Listening on port', port)
+  console.log('Listening on port', port)
 })
