daec9dff889a6327d8963e5cba198c55e6fb0881
Mikael Gråborg <mikael.graborg@iteam.se>
2018-10-13 21:43:41 +0200

    refactor: break away makeBet from routes.js

--- routes.js
+++ routes.js
@@ -7,24 +7,13 @@ const {
   getDataFromSportsRadar,
   addBetSuccess
 } = require('./lib/services/apiUtils')
-const moment = require('moment-timezone')
+const { makeBet } = require('./lib/services/bets')
 function registerRoutes (app) {
-
   app.get('/', (req, res) => {
     res.send(':D')
   })
 
-  app.post('/bets', validateToken, async (req, res) => {
-    let { body: { gameId, teamName, outcome }, username } = req
-    const { week } = await dbAdapter.getGame(gameId)
-    const { scheduled } = await dbAdapter.getGame(gameId)
-    const gameDeadline = moment.tz(scheduled, 'Europe/Stockholm').subtract(1, 'hour')
-    if (moment().isAfter(gameDeadline)) {
-      return res.status(500).send('The deadline for this bet has closed')
-    }
-    return dbAdapter.updateBet(username, gameId, teamName, outcome, week)
-      .then(() => res.sendStatus(200))
-  })
+  app.post('/bets', validateToken, makeBet)
 
   app.get(`/bets`, validateToken, (_req, res) => {
     return dbAdapter.getAllbets()
@@ -36,9 +25,9 @@ function registerRoutes (app) {
       })
   })
 
-  app.get(`/games`, validateToken, async(_req, res) => {
+  app.get(`/games`, validateToken, async (_req, res) => {
     if (await dataCollectedToday() || config.get('env') === 'DEVELOPMENT') {
-      console.log('getting data locally instead of from sportradar');
+      console.log('getting data locally instead of from sportradar')
 
       return dbAdapter.getGamesByWeek()
         .then(games => games.reduce(formatGamesFromUrl, {}))
@@ -57,12 +46,11 @@ function registerRoutes (app) {
   })
 
   app.post('/auth', (req, res, next) => {
-    res.cookie('token', req.body.token, {secure: false})
+    res.cookie('token', req.body.token, { secure: false })
     res.sendStatus(200)
     next()
   })
 }
-
 
 module.exports = {
   registerRoutes
